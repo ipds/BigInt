41de8df732b611e90bf35e8d1596e3c8221fd156
Adam Szokalski <szokalskiadam@gmail.com>
2018-11-07 18:10:19 +0100

    Tests

--- src/BigInt.h
+++ src/BigInt.h
@@ -153,6 +153,7 @@ std::ostream &operator << (std::ostream &os, BigInt const &m);              //Ov
 
 std::istream & operator >> (std::istream &in,  BigInt &c);                  //Overloads operator>> to cin the value of BigInt.SetString(in)
 
+bool operator==(const BigInt &x, const BigInt &y);                          //Overloads operator= to compare types BigInt and BigInt
 
 BigInt operator+(const BigInt &x, const BigInt &y);                         //Overloads operator+ to add types BigInt and BigInt
 
---
+++ test/addition_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include "../src/BigInt.h"
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+    cout<<argc;
+    if(argc >= 2) {
+        BigInt x(argv[0]);
+        BigInt y(argv[1]);
+        cout << x << " + " << y << " = "<< x + y << endl;
+    }
+    return 0;
+}
