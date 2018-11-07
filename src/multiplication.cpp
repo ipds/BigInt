485997192d238493dabbcfe9e8bec89dfd6c9794
Adam Szokalski <szokalskiadam@gmail.com>
2018-11-07 18:26:57 +0100

    Tests

--- src/multiplication.cpp
+++ src/multiplication.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <utility>
-#include <vector>
+#include <algorithm>
 
 #include "BigInt.h"
 
@@ -14,8 +14,8 @@ Multiprecision BigInt::Multiply(Multiprecision x, Multiprecision y) const {
     uint_fast8_t carry = 0;
     Multiprecision fraction{};
     Multiprecision res{0};
-    reverse(x.begin(), x.end());
-    reverse(y.begin(), y.end());
+    std::reverse(x.begin(), x.end());
+    std::reverse(y.begin(), y.end());
     for(unsigned int i = 0; i < x.size(); ++i)
     {
         for (auto &y_elem : y)
@@ -31,7 +31,7 @@ Multiprecision BigInt::Multiply(Multiprecision x, Multiprecision y) const {
             }
         }
         if(carry){ fraction.push_back(carry); carry = 0; }
-        reverse(fraction.begin(), fraction.end());
+        std::reverse(fraction.begin(), fraction.end());
         for(unsigned int j = 0; j < i; ++j){
             fraction.push_back(0);
         }
